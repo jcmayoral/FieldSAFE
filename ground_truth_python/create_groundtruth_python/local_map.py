e2a9c75edfdfd178e146ab5b558dbe210934cbbd
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2019-09-18 17:50:47 +0200

    rgb image :

--- ground_truth_python/create_groundtruth_python/local_map.py
+++ ground_truth_python/create_groundtruth_python/local_map.py
@@ -12,57 +12,55 @@ class Lidar2Image:
     def __init__(self, save_image=False):
         rospy.init_node("lidar_to_image")
         self.save_image = save_image
-        self.transformed_image = Image()
-        self.transformed_image.header.frame_id = "velodyne"
         self.x_resolution = 10
         self.y_resolution = 10
 
         self.bridge = CvBridge()
         self.counter = 1
-
         self.pixels_number = 1024
-        self.transformed_image.height = self.pixels_number
-        self.transformed_image.width = self.pixels_number
-        self.transformed_image.encoding = "8UC1"
-        self.transformed_image.step = self.pixels_number/8
 
         self.max_value = 255
         self.step = 25
 
         self.size =  int(self.pixels_number*self.pixels_number)
-        self.transformed_image.data = [0] * self.size
 
-        self.publisher = rospy.Publisher("/scan_velodyne_hack/image_raw", Image, queue_size=1)
         rospy.Subscriber("/velodyne_points", PointCloud2, self.topic_cb, queue_size=100)
         rospy.loginfo("Node initialized")
         rospy.spin()
 
-    def save_image_to_file(self):
+    def save_image_to_file(self,img):
+        name = str(self.counter) #self.transformed_image.header.stamp
+        #cv2_img.dtype='uint8'
+        cv2.imwrite(''+name+'.jpeg', img)
+        self.counter += 1
+
+    def ros_to_cv(self):
         try:
             # Convert your ROS Image message to OpenCV2
             #self.transformed_image.data = np.array(self.transformed_image.data)
             #self.transformed_image.data = np.round(self.transformed_image.data/self.max_value)
-            print(np.any(np.isnan(self.transformed_image.data)))
-            print(np.max(self.transformed_image.data))
             self.transformed_image.data = np.array(self.transformed_image.data)
 
             #cv2_img = self.bridge.imgmsg_to_cv2(Image(),"mono8")
-            cv2_img = self.bridge.imgmsg_to_cv2(self.transformed_image,"8UC1")
+            cv2_img = self.bridge.imgmsg_to_cv2(self.transformed_image,"rgb8")
         except CvBridgeError, e:
             print(e)
             return
-        # Save your OpenCV2 image as a jpeg
-        print ("sAVING")
-        name = str(self.counter) #self.transformed_image.header.stamp
-        #cv2_img.dtype='uint8'
-        cv2.imwrite(''+name+'.jpeg', cv2_img.astype('uint8')*255)
-        self.counter += 1
+        return cv2_img
 
 
     def topic_cb(self,msg):
-        self.transformed_image.data = [False] * self.size
+        #self.transformed_image.data = [False] * self.size * 3
 
         gen = pc2.read_points(msg, skip_nans=True, field_names=("x", "y", "z"))
+
+        #cvMat = cv2.CreateImage(self.pixels_number, self.pixels_number, cv.CV_32FC3)
+        #cvMat = cv2.Mat(2,2, CV_8UC3, Scalar(0,0,255));
+
+        rgb_color=(255, 255, 255)
+        cvMat = np.zeros((self.pixels_number, self.pixels_number, 3), np.uint8)
+        color = tuple(reversed(rgb_color))
+        cvMat[:] = color
 
         for i in range(self.size-1, -1, -1):
             try:
@@ -70,41 +68,42 @@ class Lidar2Image:
                 x = point[0]
                 y = point[1]
                 z = point[2]
-                cell_x = round(x*self.x_resolution)
-                cell_y = round(y*self.y_resolution)
+                cell_x = int(x*self.x_resolution)
+                cell_y = int(y*self.y_resolution)
                 feature = z#*point[0]*point[1]
             except:
                 continue
 
-            cell_x = (self.pixels_number/2) + cell_x
+            cell_x = int(self.pixels_number/2) + cell_x
 
             #if cell_x > self.ray_number:
             #    continue
 
-            cell_y = (self.pixels_number/2) + cell_y
+            cell_y = int(self.pixels_number/2) + cell_y
             #if cell_y > self.ray_number:
             #    continue
 
-            index =  int(fabs(cell_x+ self.transformed_image.height * cell_y))
-            if self.size - index < 0:
+            #index =  int(fabs(cell_x+ self.transformed_image.height * cell_y))
+            #if self.size - index < 0:
+            #    continue
+
+            if cell_x > self.pixels_number-1 or cell_y > self.pixels_number-1:
                 continue
 
+            if cell_x < 0 or cell_y < 0:
+                continue
             probability = 1 /fabs(feature)
-            print probability
             #update = min(fabs(feature), self.max_value)
 
 
-            if fabs(feature) < 0.5:
-                self.transformed_image.data[index] = True
-            else:
-                self.transformed_image.data[index] = False
-
-        self.transformed_image.header.stamp = rospy.Time.now()
-        self.publisher.publish(self.transformed_image)
+            if True:#z > -0.5:
+                cvMat[cell_x,cell_y,0] = 0
+                cvMat[cell_x,cell_y,1] = 0
+                cvMat[cell_x,cell_y,2] = 255
 
         if self.save_image:
             print "to save"
-            self.save_image_to_file()
+            self.save_image_to_file(cvMat)
 
 if __name__ == '__main__':
     Lidar2Image(save_image=True)
